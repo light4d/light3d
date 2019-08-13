1784525bd6385a60efa025727597343827ca8bac
timeloveboy <734991033@qq.com>
2019-08-13 23:26:11 +0800

    GEOMETRY

--- src/geometry/square.js
+++ src/geometry/square.js
@@ -4,7 +4,7 @@ import {vec3} from "gl-matrix";
 export class Square {
     /*
      */
-    constructor(a,type=WebGLRenderingContext.LINES){
+    constructor(a){
         this.position=[];
         this.position.push(vec3.scale([],[1.0,1,0],a));
         this.position.push(vec3.scale([],[1.0,-1,0],a));
@@ -16,14 +16,19 @@ export class Square {
         this.color.push([0,1,0,1]);
         this.color.push([0,1,0,1]);
         this.color.push([0,1,0,1]);
-
+    }
+    index(type=WebGLRenderingContext.LINES){
         switch (type) {
             case WebGLRenderingContext.POINTS:
                 this.index=[0,1,2,3];
             case WebGLRenderingContext.LINES://绘制一系列单独线段。每两个点作为端点，线段之间不连接。
                 this.index=[[0,1],[2,3]];
+            case WebGLRenderingContext.LINE_LOOP://绘制一个线圈。即，绘制一系列线段，上一点连接下一点，并且最后一点与第一个点相连。
+                this.index=[[0,1],[1,2],[2,3],[3,0]];
+            case WebGLRenderingContext.LINE_STRIP://绘制一个线条。即，绘制一系列线段，上一点连接下一点。
+                this.index=[[0,1],[1,2],[2,3]];
+            case WebGLRenderingContext.TRIANGLES://绘制一系列三角形。每三个点作为顶点。
+                this.index=[[0,1,2],[2,3,0]];
         }
-
     }
-
 }
